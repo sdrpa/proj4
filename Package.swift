41513e08c51fef5acf4c8e97cec45e6e40c12510
Sinisa Drpa <sdrpa@tagtaxa.com>
2017-04-18 11:31:43 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+/**
+ Created by Sinisa Drpa on 4/11/17.
+
+ PROJ4 is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or any later version.
+
+ PROJ4 is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with PROJ4.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+import PackageDescription
+
+let package = Package(
+    name: "PROJ4"
+)
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# PROJ4 - [**PROJ4**](https://github.com/OSGeo/proj.4) (Cartographic Projections Library) module.
+
+# Contributions
+
+If you would like to contribute, please fork and create a pull request on: [http://github.com/sdrpa/proj4](http://github.com/sdrpa/proj4).
